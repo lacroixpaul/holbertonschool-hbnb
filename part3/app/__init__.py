90ea2f79c7bd519ff977b648a7a140b97e05dbe4
alexandremorin24 <9761@holbertonstudents.com>
2025-03-10 13:23:18 +0100

    Refactored repository layer to use SQLAlchemy

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
+from config import DevelopmentConfig
 
-def create_app(config_class="config.DevelopmentConfig"):
+db = SQLAlchemy()
+
+def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(config_class)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
@@ -14,4 +18,5 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    db.init_app(app)
     return app
