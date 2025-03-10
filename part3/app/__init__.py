816933b3cfcefee2c0ea76a1a123ef1b5b2e99e4
alexandremorin24 <9761@holbertonstudents.com>
2025-03-10 13:23:18 +0100

    Refactored repository layer to use SQLAlchemy

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,14 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
+from config import DevelopmentConfig
 from flask_bcrypt import Bcrypt
 
 bcrypt = Bcrypt()
 
-def create_app(config_class="config.DevelopmentConfig"):
+db = SQLAlchemy()
+
+def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(config_class)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
--- part3/app/services/facade.py
+++ part3/app/services/facade.py
@@ -1,4 +1,5 @@
 from app.persistence.repository import InMemoryRepository
+from app.persistence.repository import SQLAlchemyRepository
 from app.models.user import User
 from app.models.amenity import Amenity
 from app.models.place import Place
@@ -6,28 +7,28 @@ from app.models.review import Review
 
 class HBnBFacade:
     def __init__(self):
-        self.user_repo = InMemoryRepository()
-        self.amenity_repo = InMemoryRepository()
-        self.place_repo = InMemoryRepository()
-        self.review_repo = InMemoryRepository()
+        self.user_repo = SQLAlchemyRepository(User)
+        self.place_repo = SQLAlchemyRepository(Place)
+        self.review_repo = SQLAlchemyRepository(Review)
+        self.amenity_repo = SQLAlchemyRepository(Amenity)
 
     # USER
     def create_user(self, user_data):
         user = User(**user_data)
-        self.user_repo.add(user)
+        self.user_repository.add(user)
         return user
     
     def get_users(self):
-        return self.user_repo.get_all()
+        return self.user_repository.get_all()
 
     def get_user(self, user_id):
-        return self.user_repo.get(user_id)
+        return self.user_repository.get(user_id)
 
     def get_user_by_email(self, email):
-        return self.user_repo.get_by_attribute('email', email)
+        return self.user_repository.get_by_attribute('email', email)
     
     def update_user(self, user_id, user_data):
-        self.user_repo.update(user_id, user_data)
+        self.user_repository.update(user_id, user_data)
     
     # AMENITY
     def create_amenity(self, amenity_data):
